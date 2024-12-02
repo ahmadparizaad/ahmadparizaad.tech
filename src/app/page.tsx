2cc008053334803e9acbe36105f8a30f75033d9c
Mohammad Ahmad <104555382+ahmadparizaad@users.noreply.github.com>
2024-12-03 03:22:09 +0530

    Remove unused import of useEffect from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Skills from '../components/Skills';
 import Projects from '../components/Projects';
 import Experience from '../components/Experience';
 import Contact from '../components/Contact';
-import { useEffect } from 'react';
 import SmoothScroll from '../components/SmoothScroll';
 import Navigation from '../components/Navigation';
 
